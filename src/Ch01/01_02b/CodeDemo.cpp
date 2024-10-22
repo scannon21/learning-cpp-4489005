6994ac5d3cd1264c363b58c30e3d82c7dc86c7b6
scannon21 <158185201+scannon21@users.noreply.github.com>
2024-10-22 18:40:50 +0000

    Hello Worl

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hi There!" << endl;
+    cout << endl << endl;
+    return(0);
+}
