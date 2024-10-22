b865520fc09f1617988d4a0ba0e92fc50269313d
scannon21 <158185201+scannon21@users.noreply.github.com>
2024-10-22 21:11:51 +0000

    Integers

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -3,10 +3,34 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
+using namespace std;
+
+int a, b = 5; // single lne comment
+
+/* 
+Multi
+Line
+Comment
+*/
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 7;
+
+    my_flag = false;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "flag = " << my_flag << endl;
+
+    my_flag = true;
+    cout << "flag = " << my_flag << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "b - a = " << b - a << endl;
     
-    std::cout << std::endl << std::endl;
+    unsigned int positive;
+    positive = b - a;
+    cout << "b - a (unsigned) = " << positive << endl;
+    
+    cout << endl << endl;
     return (0);
 }
