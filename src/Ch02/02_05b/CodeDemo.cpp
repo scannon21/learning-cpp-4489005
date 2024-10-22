32514c73d82f3dbdfa12819b97a7a38b5abdd7a6
scannon21 <158185201+scannon21@users.noreply.github.com>
2024-10-22 21:21:17 +0000

    Variable Types

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,9 +4,25 @@
 
 #include <iostream>
 #include <typeinfo>
+using namespace std;
 
 int main(){
+    auto a = 8;
+    auto b = 12345678901;
+    auto c = 3.14f;
+    auto d = 3.14;
+    auto e = true;
+    auto f = 'd';
+    auto g = "C++ is a language";
     
-    std::cout << std::endl << std::endl;
+    cout << "The type of a is " << typeid(a).name() << endl;
+    cout << "The type of b is " << typeid(b).name() << endl;
+    cout << "The type of c is " << typeid(c).name() << endl;
+    cout << "The type of d is " << typeid(d).name() << endl;
+    cout << "The type of e is " << typeid(e).name() << endl;
+    cout << "The type of f is " << typeid(f).name() << endl;
+    cout << "The type of g is " << typeid(g).name() << endl;
+
+    cout << endl << endl;
     return (0);
 }
